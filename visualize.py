2adfd31341f0360bd586dee2f5e510896c950d80
ranand5 <hitanand4@gmail.com>
2018-09-11 19:27:57 +0530

    adding grid to the graph and finding the value

--- visualize.py
+++ visualize.py
@@ -2,13 +2,23 @@
 Module for visualizing the dataset that is going to be used using graph.
 """
 
-from datetime import datetime
-from pandas import read_csv
+import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 import sys
-import pandas as pd
 
 data = pd.read_csv(sys.argv[1])
-# original data - closing price
-data['Adj Close'].plot(figsize=(15,8), title = 'Close Price', fontsize=14)
+
+data = data['Adj Close']
+
+n = data.min()
+m = data.max()
+data.plot(figsize=(10,6), title = 'Close Price', fontsize=14)
+
+plt.xlim(1,2200)
+plt.ylim(n,m)
+plt.xlabel('Number of data',fontsize=20)
+plt.ylabel('value',fontsize=20)
+plt.title("Datasets Visualizatoin")
+plt.grid(True)
 plt.show()
